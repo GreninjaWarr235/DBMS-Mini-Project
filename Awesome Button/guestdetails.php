44a79e2a93457f2701c7e3ee00f5c6d9848c7811
Mufaddal-Shiyaji <mufaddal1693@gmail.com>
2022-11-12 16:21:49 +0530

    php file created

---
+++ Awesome Button/guestdetails.php
@@ -0,0 +1,12 @@
+<?php
+$title = $_POST['title'];
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+$email = $_POST['email'];
+$country = $_POST['country'];
+$contactNum = $_POST['contactNum'];
+$membershipNum = $_POST['membershipNum'];
+$gstNum = $_POST['gstNum'];
+$special = $_POST['special'];
+$policy = $_POST['policy'];
+?>
